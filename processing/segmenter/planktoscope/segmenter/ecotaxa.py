040443d63793e234320a7154458496b91f29be26
oumayma-hy <elbezoumayma123@gmail.com>
2024-07-24 09:31:19 +0100

    Using the acquisition ID in the filename of the EcoTaxa metadata & export files

--- processing/segmenter/planktoscope/segmenter/ecotaxa.py
+++ processing/segmenter/planktoscope/segmenter/ecotaxa.py
@@ -283,15 +283,19 @@ def ecotaxa_export(archive_filepath, metadata, image_base_path, keep_files=False
             list(zip(tsv_content.columns, tsv_type_header))
         )
 
+        # create the filename with the acquisition ID
+        acquisition_id = metadata.get("acq_id")
+        tsv_filename = f"ecotaxa_{acquisition_id}.tsv"
+
         # add the tsv to the archive
         archive.writestr(
-            "ecotaxa_export_modified.tsv",
+            tsv_filename,
             io.BytesIO(
                 tsv_content.to_csv(sep="\t", encoding="utf-8", index=False).encode()
             ).read(),
         )
         if keep_files:
-            tsv_file = os.path.join(image_base_path, f"ecotaxa_export_modified.tsv")
+            tsv_file = os.path.join(image_base_path, tsv_filename)
             tsv_content.to_csv(
                 path_or_buf=tsv_file, sep="\t", encoding="utf-8", index=False
             )
